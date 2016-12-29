cce69c2a418eb18d904d4a18e5b770ff073b9f1e
Andrew Tachilin <andrewtachilin@gmail.com>
2016-12-29 15:24:55 +0200

    correction name of definies and correction work script

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 define("SKILL_POINT", 1);
-define("MAXIMUM_SCORE_OF_POINT", 39);
-define("MAXIMUM_POINT_FOR_SKILL",9);
+define("MAXIMUM_SCORE_FOR_ABILITIES", 39);
+define("MAXIMUM_SCORE_POINT_FOR_ONE_SKILL",9);
 
 
 function Spectial($ArrayOfSkills)
@@ -29,20 +29,31 @@ function Spectial($ArrayOfSkills)
         '1'
 
     ];
-$resultOfPoint=1;
+
+
 //checking for sum of point,  must be under 39
-    while ($resultOfPoint <= MAXIMUM_SCORE_OF_POINT) {
+    $resultOfPoint = SKILL_POINT;
+    while ($resultOfPoint <= MAXIMUM_SCORE_FOR_ABILITIES) {
         //take array of point and get sum for validate our while
         $resultOfPoint = array_sum($specialPoint);
         //Genereate a number, that ability we will increase
         $randomAbility = rand(0, 6);
         //the point must me under or equal 10 if true add one skill poit for this ability
-        if ($specialPoint[$randomAbility] <= MAXIMUM_POINT_FOR_SKILL) {
+        if ($specialPoint[$randomAbility] <= MAXIMUM_SCORE_POINT_FOR_ONE_SKILL
+            && $resultOfPoint <= MAXIMUM_SCORE_FOR_ABILITIES ) {
+
             $specialPoint[$randomAbility] += SKILL_POINT;
         }
+
     }
+
 //merge 2 array on 1
     $ArrayOfSkills = array_combine($specialAbility, $specialPoint);
+
     return $ArrayOfSkills;
+
 }
+
+
+
 print_r(Spectial($ArrayOfSkills));
